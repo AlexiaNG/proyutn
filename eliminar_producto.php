96bf12f58774ff6d17b024023082f4b700f852b8
AlexiaNG <alexia@glibota.com>
2014-03-17 22:13:02 -0300

    Inicializando repo

---
+++ eliminar_producto.php
@@ -0,0 +1,17 @@
+<?php
+
+	require_once 'core/init.php';
+	
+	$id = $_GET["id"];
+	if(!empty($id)){
+		$sql = "DELETE FROM productos WHERE id = ?";
+		$query = DB::getInstance()->consultar($sql,array($id));
+		if($query->error()){
+			//error
+			header("Location: listar_producto.php");
+		}else{
+			//ok
+			header("Location: listar_producto.php");
+		}
+	}
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Pantalla de inicio</title>
+</head>
+<body>
+	<a href="listar_producto.php">Listar Productos</a>
+	<br>
+	<a href="agregar_producto.php">Agregar Productos</a>
+	<br>
+ 	<a href="registrar_usuario.php">Registrar Usuario</a>
+</body>
+</html>
